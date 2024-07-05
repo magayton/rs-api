c290922dbfa2e742104d26df1a808b538e720683
Mathis Gayton <mathis.gayton@gmail.com>
2024-07-05 18:34:34 +0200

    Create service for add calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use proto::calculator_server::{Calculator, CalculatorServer};
+
+mod proto {
+    tonic::include_proto!("calculator");
+}
+
+#[derive(Debug, Default)]
+struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        println!("Got a request: {:?}", request);
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationResponse {
+            result: input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
